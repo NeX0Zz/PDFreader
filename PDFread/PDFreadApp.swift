082983a85b2f60d9098576725f27510cbf42f0e0
Denis Nikolaev <150382827+NeX0Zz@users.noreply.github.com>
2025-02-12 10:52:03 +0300

    Initial Commit

---
+++ PDFread/PDFreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFreadApp.swift
+//  PDFread
+//
+//  Created by Денис Николаев on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
